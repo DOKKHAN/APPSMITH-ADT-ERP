f508662e283740a3ded6f04551e486f4386e92c5
Victor Contreras <gimnasio@adarlotodo.cl>
2026-05-29 17:38:21 +0000

    cambios de opacidad

--- pages/Login/jsobjects/Auth/Auth.js
+++ pages/Login/jsobjects/Auth/Auth.js
@@ -43,7 +43,7 @@ export default {
     async fadeSlogan(opacitySteps) {
         for (const opacity of opacitySteps) {
             await storeValue("login_slogan_opacity", opacity);
-            await new Promise(resolve => setTimeout(resolve, 45));
+            await new Promise(resolve => setTimeout(resolve, 90));
         }
     },
 
